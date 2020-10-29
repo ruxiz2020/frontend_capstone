097a4aabb379311fcf2d0e66a46150edd9200bcb
Ruxi Zhang <zruxi@vmware.com>
2020-10-29 11:07:13 -0700

    server code

---
+++ src/client/index.js
@@ -0,0 +1,19 @@
+import {handleSubmit} from "./js/handleSubmit";
+import {plan_now} from "./js/app";
+import {plan_trip} from "./js/app";
+import {remove_trip} from "./js/app";
+
+import './styles/reset.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+import './styles/style.scss'
+
+import './views/img/travel-svgrepo-com.svg'
+import './views/img/travel.jpg'
+
+export {
+  handleSubmit,
+  plan_trip,
+  plan_now,
+  remove_trip
+}
---
+++ src/server/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(8888, function() {
+  console.log("Server is running at 8888");
+});
